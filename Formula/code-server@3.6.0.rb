8f9852e070d764691e3eb4eaf933259b59a8c617
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2021-02-12 01:04:31 -0800

    initial commit

---
+++ Formula/code-server@3.6.0.rb
@@ -0,0 +1,77 @@
+class CodeServerAT360 < Formula
+  desc "Access VS Code through the browser"
+  homepage "https://github.com/cdr/code-server"
+  url "https://registry.npmjs.org/code-server/-/code-server-3.6.0.tgz"
+  sha256 "21bb3801d0c5f510d147fd30c2136e5f29cc1365ea7ddd9b9ecd578bd6302839"
+  license "MIT"
+
+  livecheck do
+    url :stable
+  end
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "05ecc38e633a7da62943a68ef0daf1b385bd7596a81a8d199cd2d099eba55956" => :catalina
+    sha256 "451f3a58a7d4fe6f7dfb2cfda0485e4df7edfe0dffd8423c185a448bbc061a8f" => :mojave
+    sha256 "396c4ee64b8337c597587463b22e72e2d5b16ac0bb942d02878b9d5655afd002" => :high_sierra
+  end
+
+  depends_on "python@3.8" => :build
+  depends_on "yarn" => :build
+  depends_on "node"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "libsecret"
+    depends_on "libx11"
+    depends_on "libxkbfile"
+  end
+
+  def install
+    system "yarn", "--production", "--frozen-lockfile"
+    libexec.install Dir["*"]
+    env = { PATH: "#{HOMEBREW_PREFIX}/opt/node/bin:$PATH" }
+    (bin/"code-server").write_env_script "#{libexec}/out/node/entry.js", env
+  end
+
+  def caveats
+    <<~EOS
+      The launchd service runs on http://127.0.0.1:8080. Logs are located at #{var}/log/code-server.log.
+    EOS
+  end
+
+  plist_options manual: "code-server"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{HOMEBREW_PREFIX}/bin/code-server</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{ENV["HOME"]}</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/code-server.log</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/code-server.log</string>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    system bin/"code-server", "--extensions-dir=.", "--install-extension", "ms-python.python"
+    assert_equal "ms-python.python\n",
+      shell_output("#{bin/"code-server"} --extensions-dir=. --list-extensions")
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# JtMotoX Versions
+
+## How do I install these formulae?
+`brew install jtmotox/versions/<formula>`
+
+Or `brew tap jtmotox/versions` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
